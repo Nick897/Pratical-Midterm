c893d0e02154d31e8d3ee28be59aef51806df04b
Nivica8971 <nicholas.xynos@ontariotechu.net>
2023-03-09 13:28:58 -0500

    Canvas setup for client 2

---
+++ Networking Midterm Client 2/Assets/Scripts/OverlayUpdate.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class OverlayUpdate : MonoBehaviour
+{
+    private Text myText;
+    private static float position;
+   
+    public static void UpdatePos(float pos)
+    {
+        position = pos;
+     
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GetComponent<Text>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myText.text = "Position X: " + position;
+      
+
+    }
+}
---
+++ Networking Midterm Client 2/Assets/Scripts/ServerIP.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ServerIP : MonoBehaviour
+{
+    public string serverIP;
+    public GameObject inputText;
+    public GameObject displayMsg;
+
+    public void SetServerIP()
+    {
+        // Get text from input field 
+
+        serverIP = inputText.GetComponent<Text>().text;
+
+        displayMsg.GetComponent<Text>().text = "Server IP: " + serverIP;
+    }
+
+
+}
